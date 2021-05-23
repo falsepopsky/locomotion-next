ac4abe1b136c979b265c62cc9110a75bc6b59a00
falsepopsky <javicream@gmail.com>
2021-05-22 23:07:35 -0300

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,25 @@
-import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
+
+  *, ::after, ::before {
+  box-sizing: border-box;
+  }
+
   body {
     margin: 0;
     padding: 0;
-    box-sizing: border-box;
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
   }
-`
+`;
 
 const theme = {
   colors: {
     primary: '#0070f3',
   },
-}
+};
 
 export default function App({ Component, pageProps }) {
   return (
@@ -22,5 +29,5 @@ export default function App({ Component, pageProps }) {
         <Component {...pageProps} />
       </ThemeProvider>
     </>
-  )
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,16 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
+import Meta from '../components/Meta';
 
 const Title = styled.h1`
   font-size: 50px;
   color: ${({ theme }) => theme.colors.primary};
-`
+`;
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <>
+      <Meta />
+      <Title>My page</Title>
+    </>
+  );
 }
